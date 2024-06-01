014a0f3a81899dd3dc6b45e54d2fbc53bdcc58d1
Francis Iwu <apkfem@gmail.com>
2024-06-01 21:29:16 +0200

    implement change of meta settings in the main layout.tsx

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,9 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const LeftSidebar = () => {
   return (
-    <section>
-      <nav className="left_sidebar"></nav>
+    <section className="left_sidebar">
+      <nav className="flex flex-col gap-6">
+        <Link href={"/"}>
+          <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
+        </Link>
+      </nav>
     </section>
   );
 };
